8f7059fb1c19a3b56274b57c785b49dd9a317c03
Danny Warren <danny.warren@emmersionlearning.com>
2020-03-26 19:14:53 -0600

    Use process.env.PORT to work with Heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000
 
 const customersRoutes = require('./Routes/mongoCustomers')
 const vehiclesRoutes = require('./Routes/vehicles')
